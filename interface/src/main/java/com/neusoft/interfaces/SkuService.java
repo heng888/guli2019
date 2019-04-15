8e2b374b5c72d262178934e5bfbb51c9c7c17793
heng888 <binheng716>
2019-04-15 10:31:56 +0800

    0415

---
+++ interface/src/main/java/com/neusoft/interfaces/SkuService.java
@@ -0,0 +1,14 @@
+package com.neusoft.interfaces;
+
+import com.neusoft.javabean.po.SkuInfo;
+
+import java.util.List;
+
+public interface SkuService {
+    List<SkuInfo> getSkuListBySpu(Long spuId);
+    //查询skuInfo相关的spuImage、spuAttrValue、spuSaleAttrValue
+    List<SkuInfo> selectSkuInfoBySpuId(Long spuId);
+
+    //新增skuInfo
+    void saveSku(SkuInfo skuInfo);
+}
